8c34db16d64840daaf82bbc3244f049a6aa7f91c
MrLJones <liam.andrew.jones@gmail.com>
2015-10-24 18:49:02 +0100

    Working

--- Emote/app/src/main/java/com/example/liam/emote/myApplication.java
+++ Emote/app/src/main/java/com/example/liam/emote/myApplication.java
@@ -2,6 +2,16 @@ package com.example.liam.emote;
 
 import android.app.Application;
 import com.estimote.sdk.BeaconManager;
+import com.estimote.sdk.Region;
+import android.app.*;
+import android.content.*;
+import android.widget.Toast;
+import android.widget.TextView;
+
+import com.estimote.sdk.Beacon;
+import java.util.List;
+
+import java.util.UUID;
 
 public class myApplication extends Application {
 
@@ -11,5 +21,52 @@ public class myApplication extends Application {
     public void onCreate() {
         super.onCreate();
         beaconManager = new BeaconManager(getApplicationContext());
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+                beaconManager.startMonitoring(new Region("monitored region", null,null,null));
+                Toast.makeText(getApplicationContext(), "Monitoring", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        beaconManager.setMonitoringListener(new BeaconManager.MonitoringListener() {
+            @Override
+            public void onEnteredRegion(Region region, List<Beacon> list) {
+                String content = "";
+                for(Beacon beacon : list) {
+                    content = content + beacon.getMacAddress() + ", ";
+                }
+                
+                Toast.makeText(getApplicationContext(),region.getIdentifier() +
+                        ", " + content ,Toast.LENGTH_LONG).show();
+               showNotification("You have entered a battle", "You have entered a battle with a monster click to fight and check it out.");
+            }
+
+            @Override
+            public void onExitedRegion(Region region) {
+                Toast.makeText(getApplicationContext(), "Exited a beacon", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void showNotification(String title, String message) {
+        Intent notifyIntent = new Intent(this, MainActivity.class);
+        notifyIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivities(this, 0,
+                new Intent[] { notifyIntent }, PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+        Notification notification = new Notification.Builder(this)
+                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .build();
+
+        notification.defaults |= Notification.DEFAULT_SOUND;
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, notification);
     }
 }
